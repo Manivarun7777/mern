4bfbb1574223986c948834b10aaac0977a421413
manivarun7777 <mani7777varun@gmail.com>
2025-03-13 15:45:07 +0530

    message

---
+++ routes/itemRoutes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/Item');
+
+router.post('/', async (req, res) => {
+    try {
+        const item = new Item(req.body);
+        await item.save();
+        res.status(201).json(item);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (!item) return res.status(404).json({ message: 'Item not found' });
+        res.json(item);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!item) return res.status(404).json({ message: 'Item not found' });
+        res.json(item);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const item = await Item.findByIdAndDelete(req.params.id);
+        if (!item) return res.status(404).json({ message: 'Item not found' });
+        res.json({ message: 'Item deleted' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = router;
